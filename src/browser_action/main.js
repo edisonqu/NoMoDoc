9021f98da1115ffd9558d2f1dec5fefe027c0d7b
edison <edisonquu@gmail.com>
2022-11-18 18:28:56 -0500

    Initial commit

---
+++ src/browser_action/main.js
@@ -0,0 +1,7 @@
+
+document.getElementById("#submit").addEventListener("click", onSubmit());
+
+async function onSubmit(){
+    let question = document.getElementByTagName("input")
+    console.log(question)
+}
