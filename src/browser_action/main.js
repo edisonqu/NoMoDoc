d05ad6ff2e421454496a9f8426a5dc1bd4a88da2
edison <edisonquu@gmail.com>
2022-11-18 22:01:16 -0500

    was able to put content onto the extension

--- src/browser_action/main.js
+++ src/browser_action/main.js
@@ -6,5 +6,8 @@ function onSubmit(event){
     event.preventDefault()
     let question = document.getElementById('inputted').value
     const output = document.getElementById('output')
-
+    var theDiv = document.getElementById("output");
+    var content = document.createTextNode(question);
+    theDiv.appendChild(content)
 }
+
